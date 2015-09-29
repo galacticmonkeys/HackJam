367a05728fea949c66185e8f369c9afb24de2839
Jacob Young <jacob@jacobyoung.me>
2015-09-29 10:19:53 -0700

    Add secret emu gif
    Type in 'emu' into the page and the emu gif will display.
    Need to remove the gif after 14 seconds.
    Also need to add note to the footer.

--- js/index.js
+++ js/index.js
@@ -7,4 +7,29 @@ function showIFrame() {
  iframe.onload = function() {
    iframe.contentWindow.focus();
  };
-}  
+}
+(function() {
+  var targetString = 'emu';
+  var currentString = '';
+  document.addEventListener("keydown", function(e){
+    if(e.keyCode === 69 ) {
+      currentString += 'e';
+      console.log(e.keyCode);
+      console.log(currentString);
+    }
+    if(e.keyCode === 77) {
+      currentString += 'm';
+      console.log(currentString);
+    }
+    if(e.keyCode === 85) {
+      currentString += 'u';
+      console.log(currentString);
+    }
+    
+    if(currentString === targetString) displayEmus();
+  }, true);
+
+  function displayEmus() {
+    document.getElementById('emu').style.display = 'block';
+  }
+})();
